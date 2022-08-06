c7e9669c3b21886b941b91eca4da0513bf762330
Renata Lucena <101506069+Renatalucena@users.noreply.github.com>
2022-08-06 22:53:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    String texto = " manipulando  strings!  ";
+    System.out.println(texto.length());
+    System.out.println(texto +"-EXERCÍCIO-");
+     System.out.println(texto.indexOf("man"));
+    String email = " renata@gmail.com  ";
+    System.out.println(email.trim());
+    System.out.println(texto.contains(".gmail"));
+    System.out.println(texto.indexOf("man"));
+    String curso = " programação orienTADA a OBJETOS"; 
+    System.out.println(curso.toUpperCase());
+    System.out.println(curso.toLowerCase());
+   
+  }
+}
